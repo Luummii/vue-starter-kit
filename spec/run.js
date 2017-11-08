dff1f0dd4ada2540fac92507fa6ce4beed4b9867
Andrey Zimin <zimin6077@gmail.com>
2017-11-08 17:25:18 +0300

    Change name tests run

--- spec/run.js
+++ spec/run.js
@@ -1,2 +1,6 @@
-require('babel-register')
-require('./test')
+import Jasmine from 'jasmine'
+
+const jasmine = new Jasmine()
+
+jasmine.loadConfigFile('spec/support/jasmine.json')
+jasmine.execute()
---
+++ start.sh
@@ -0,0 +1,2 @@
+# start production
+pm2 start ecosystem.config.js
