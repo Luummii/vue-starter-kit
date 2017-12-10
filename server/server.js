25c0273bef9f898f8870010f1c273e73c9689d5e
Andrey Zimin (luummii) <luummii@vuejs.fun>
2017-12-10 20:54:17 +0300

    Add winston in listen mesage

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 import Koa from 'koa'
 import middlewares from './middlewares'
 import configServer from '../config/server'
+import winston from 'winston-color'
 
 const app = new Koa()
 
 middlewares.forEach((middleware) => middleware(app))
+
+winston.warn(`Server is listening port ${configServer.port}`)
 
 // For testing
 export default app.listen(configServer.port)
